fffcd7508a93d275c4d38c530c7dd45afabe1ea9
Rajesh Khadka <rajesh.k.khadka@gmail.com>
2022-11-23 23:11:26 +0100

    change method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def generate_password():
-    pass
+def add(a: int, b: int) -> int:
+    return a + b
 
 
-if __name__ == '__main__':
-    generate_password()
+if __name__ == "__main__":
+    add(2, 4)
